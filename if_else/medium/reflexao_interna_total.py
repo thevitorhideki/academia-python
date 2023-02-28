85bac82ac7b069788e6e0faea963d7a81cedddf9
Phvr06 <66474099+Phvr06@users.noreply.github.com>
2023-02-28 13:38:29 -0300

    Update reflexao_interna_total.py

--- if_else/medium/reflexao_interna_total.py
+++ if_else/medium/reflexao_interna_total.py
@@ -1,10 +1,7 @@
 import math
-
-def reflexao_total_interna(n1, n2, teta2):
-    teta1 = math.asin(math.radians((n2 * math.sin(math.radians(teta2))) / n1))
-    limite = math.asin(math.radians(n1 / n2))
-    
-        if teta1 > limite:
+def reflexao_total_interna (n1, n2, teta2):
+    limite = math.asin(n1/n2)
+    if math.radians(teta2) > limite:
         return True
-    
-    return False
+    else:
+        return False
