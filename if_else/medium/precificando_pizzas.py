5a45503c12634a8e780d84c07cc9e22b36b74699
Enzo Junqueira <124401185+enzojunq@users.noreply.github.com>
2023-02-28 12:58:00 -0300

    Update precificando_pizzas.py

--- if_else/medium/precificando_pizzas.py
+++ if_else/medium/precificando_pizzas.py
@@ -5,20 +5,32 @@ elif pergunta1=='M':
     valor=70.0
 elif pergunta1=='G':
     valor=90.0
+
 pergunta2=input("Borda recheada [S/N]? ")
-if pergunta2=='S':
-    valor=valor*1.15
+
 pergunta3=input("Adicional de queijo [S/N]? ")
-if pergunta3=='S':
-    if pergunta2=='S':
-        valor
-    valor=1.1*valor
+
 pergunta4=input("Refrigerante [S/N]? ")
+
+pergunta5=input("Sobremesa [S/N]? ")
+
+if pergunta2 == 'S' and pergunta3=='S':
+    valor=valor*1.25
+elif pergunta2 == 'S' and pergunta3!='S':
+    valor=valor*1.15
+elif pergunta2!='S' and pergunta3=='S':
+    valor=valor*1.10
+
 if pergunta4=='S':
     valor+=12
-pergunta5=input("Sobremesa [S/N]? ")
+
 if pergunta5=='S':
-    valor+=20
     if pergunta1=='G':
-        valor=valor*0.93
+        valor+=20
+        valor=0.93*valor
+    else:
+        valor+=20
+        
+
+
 print(f'{valor:.2f}')
