d268856bbd9787660e42809da2ae3b5ef17e7337
Vitor Katakura <55566176+TheVitorGomez@users.noreply.github.com>
2023-02-28 13:33:04 -0300

    Update reflexao_interna_total.py

--- if_else/medium/reflexao_interna_total.py
+++ if_else/medium/reflexao_interna_total.py
@@ -1,8 +1,10 @@
 import math
 
 def reflexao_total_interna(n1, n2, teta2):
-    teta1 = math.asin(math.sin(math.radians(teta2)) * n2 / n1)
-    if teta1 > 90:
+    teta1 = math.asin(math.radians((n2 * math.sin(math.radians(teta2))) / n1))
+    limite = math.asin(math.radians(n1 / n2))
+    
+        if teta1 > limite:
         return True
-    else:
-        return False
+    
+    return False
