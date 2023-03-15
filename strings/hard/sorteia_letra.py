7b03602c7aea2d4aa350511a42f57800be621573
TheVitorGomez <7vitorgomez@gmail.com>
2023-03-15 12:05:21 -0300

    Code refactoring

--- strings/hard/sorteia_letra.py
+++ strings/hard/sorteia_letra.py
@@ -4,8 +4,5 @@ from random import choice
 def sorteia_letra(text: str, restriction: list) -> str:
     restriction += ['.', ',', '-', ';', ' ']
     text = [char for char in text.lower() if char not in restriction]
-    if len(text) == 0:
-        return ''
 
-    text = ''.join(text)
-    return choice(text)
+    return '' if not text else choice(text)
