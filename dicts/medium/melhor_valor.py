cfe113310ac1b0d31a63feba15ba240f04f4ed7a
Vitor Katakura <55566176+thevitorhideki@users.noreply.github.com>
2023-04-18 18:22:50 -0300

    Update melhor_valor.py

--- dicts/medium/melhor_valor.py
+++ dicts/medium/melhor_valor.py
@@ -12,11 +12,3 @@ def melhor_valor(parts: list, inventory: list[dict]) -> float:
                 break
 
     return total
-
-
-print(melhor_valor(['X1D', 'A3B'], [
-    {'serial': 'A3B', 'valor': 198.7, 'equivalente': []},
-    {'serial': 'XP2', 'valor': 190.9, 'equivalente': ['Z3Z', 'A3B']},
-    {'serial': 'XP1', 'valor':   5.1,
-        'equivalente': ['TH5', 'TH6', 'X3D', 'X1D']}
-]))
